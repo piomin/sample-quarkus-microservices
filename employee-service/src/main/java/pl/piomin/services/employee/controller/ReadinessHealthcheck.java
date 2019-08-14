faaaebc3813d9a08fc22a9c6a41d191673a2424d
minkowp <piotr.minkowski@play.pl>
2019-08-14 16:22:56 +0200

    Adding health checks

---
+++ employee-service/src/main/java/pl/piomin/services/employee/controller/ReadinessHealthcheck.java
@@ -0,0 +1,18 @@
+package pl.piomin.services.employee.controller;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+@Readiness
+public class ReadinessHealthcheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("Employee Health Check").up().build();
+    }
+
+}
